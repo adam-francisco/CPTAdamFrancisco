3521974c12aad681bebe3eb29032cb9578d6aec9
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-23 09:35:03 -0400

    Update CPTAdam.java

--- CPTAdam.java
+++ CPTAdam.java
@@ -3,7 +3,18 @@ import arc.*;
 public class CPTAdam{
 	public static void main (String[] args){
 		Console con = new Console();
-		con.println("Hello World");
-
+		
+		String strMood;
+		
+		con.println("How are you?");
+		strMood = con.readLine();
+		
+		if(strMood.equalsIgnoreCase("good")){
+			con.println("That's great");
+		}else if(strMood.equalsIgnoreCase("ok")){
+			con.println("Hopefully you feel better");
+		}else{
+			con.println("That's too bad");
+		}
 	}
 }
