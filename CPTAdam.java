c7d908546e58d5cc9811724711930c5912b7a6c2
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-30 09:24:04 -0400

    Polished main menu further with hints for button presses, proper center allignment, and movoing around strings

--- CPTAdam.java
+++ CPTAdam.java
@@ -9,14 +9,21 @@ public class CPTAdam{
 		con.setDrawColor(Color.WHITE);
 		con.fillRect(0,0,1000,600);
 		
+		con.setDrawColor(new Color(205, 205, 205));
+		con.fillRect(498,220,5,150);
+		
 		con.setDrawColor(Color.BLACK);
-		con.drawString("Blackjack",530,220);
-		con.drawString("Main Menu",530,250);
-		con.drawString("Play [P]",530,280);
-		con.drawString("Leaderboard [L]",530,310);
-		con.drawString("Quit [Q]",530,340);
+		con.drawString("Blackjack",535,220);
+		con.drawString("Main Menu",535,250);
+		con.drawString("Play [P]",535,280);
+		con.drawString("Leaderboard [L]",535,310);
+		con.drawString("Quit [Q]",535,340);
+		
+		con.setDrawColor(new Color(205, 205, 205));
+		con.drawString("* Type and enter the corresponding letter to open the option", 140, 555);
+		
 		BufferedImage imgPoker = con.loadImage("blackjack.png");
-				con.drawImage(imgPoker, 175, 150);
+				con.drawImage(imgPoker, 180, 150);
 		
 	}
 }
