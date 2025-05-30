dc04421bf0ceaedeb4865e3e9586058f8f621929
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-30 08:35:20 -0400

    Created strings for Menu Screen

--- CPTAdam.java
+++ CPTAdam.java
@@ -4,7 +4,11 @@ public class CPTAdam{
 	public static void main (String[] args){
 		Console con = new Console(1000,600);
 		
-		con.drawString("Blackjack",445,150);
+		con.drawString("Blackjack",445,300);
+		con.drawString("Main Menu",445,330);
+		con.drawString("Play [P]",453,360);
+		con.drawString("Leaderboard [L]",415,390);
+		con.drawString("Quit [Q]",453,420);
 		
 	}
 }
