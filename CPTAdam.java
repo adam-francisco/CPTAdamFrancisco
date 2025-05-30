674ff72f21858c21a5692b031cdc79233581bbcf
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-30 08:59:02 -0400

    Polished main menu and added png Logo

--- CPTAdam.java
+++ CPTAdam.java
@@ -1,14 +1,22 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
 public class CPTAdam{
 	public static void main (String[] args){
 		Console con = new Console(1000,600);
 		
-		con.drawString("Blackjack",445,300);
-		con.drawString("Main Menu",445,330);
-		con.drawString("Play [P]",453,360);
-		con.drawString("Leaderboard [L]",415,390);
-		con.drawString("Quit [Q]",453,420);
+		con.setDrawColor(Color.WHITE);
+		con.fillRect(0,0,1000,600);
+		
+		con.setDrawColor(Color.BLACK);
+		con.drawString("Blackjack",530,220);
+		con.drawString("Main Menu",530,250);
+		con.drawString("Play [P]",530,280);
+		con.drawString("Leaderboard [L]",530,310);
+		con.drawString("Quit [Q]",530,340);
+		BufferedImage imgPoker = con.loadImage("blackjack.png");
+				con.drawImage(imgPoker, 175, 150);
 		
 	}
 }
