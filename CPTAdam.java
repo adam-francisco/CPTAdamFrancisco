e1453c583ae7c052c3d91f15d60ebc7a49d07a03
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-30 08:23:29 -0400

    Update CPTAdam.java

--- CPTAdam.java
+++ CPTAdam.java
@@ -2,8 +2,9 @@ import arc.*;
 
 public class CPTAdam{
 	public static void main (String[] args){
-		Console con = new Console();
+		Console con = new Console(1000,600);
 		
+		con.drawString("Blackjack",445,150);
 		
 	}
 }
