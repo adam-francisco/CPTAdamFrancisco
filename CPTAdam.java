143a6270ec3a7a1bf83b0f0257f4a73faf7e9cb9
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-05-30 09:26:04 -0400

    fixed centering issue

--- CPTAdam.java
+++ CPTAdam.java
@@ -20,7 +20,7 @@ public class CPTAdam{
 		con.drawString("Quit [Q]",535,340);
 		
 		con.setDrawColor(new Color(205, 205, 205));
-		con.drawString("* Type and enter the corresponding letter to open the option", 140, 555);
+		con.drawString("* Type and enter the corresponding letter to open the option", 137, 555);
 		
 		BufferedImage imgPoker = con.loadImage("blackjack.png");
 				con.drawImage(imgPoker, 180, 150);
