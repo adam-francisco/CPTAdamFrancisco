777f0589d6d97e60a0997792bc117a4b0804863b
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-06-02 09:40:51 -0400

    set up game options and started designing poker table

--- CPTAdam.java
+++ CPTAdam.java
@@ -6,24 +6,47 @@ public class CPTAdam{
 	public static void main (String[] args){
 		Console con = new Console(1000,600);
 		
-		con.setDrawColor(Color.WHITE);
-		con.fillRect(0,0,1000,600);
+		char chrOption;
+		
+		con.setBackgroundColor(Color.WHITE);
 		
 		con.setDrawColor(new Color(205, 205, 205));
-		con.fillRect(495, 220, 5, 155);
+		con.fillRect(495, 210, 5, 185);
 		
 		con.setDrawColor(Color.BLACK);
-		con.drawString("Blackjack",532,220);
-		con.drawString("Main Menu",532,250);
-		con.drawString("Play [P]",532,280);
-		con.drawString("Leaderboard [L]",532,310);
-		con.drawString("Quit [Q]",532,340);
+		con.drawString("Blackjack",532,210);
+		con.drawString("Main Menu",532,240);
+		con.drawString("Play [P]",532,270);
+		con.drawString("Leaderboard [L]",532,300);
+		con.drawString("Quit [Q]",532,330);
+		con.drawString("Help [H]",532,360);
+		
+		
+		BufferedImage imgPoker = con.loadImage("blackjack.png");
+				con.drawImage(imgPoker, 180, 140);
 		
 		con.setDrawColor(new Color(205, 205, 205));
 		con.drawString("* Type the corresponding letter to open the option", 197, 555);
 		
-		BufferedImage imgPoker = con.loadImage("blackjack.png");
-				con.drawImage(imgPoker, 180, 150);
-		
+		chrOption = con.getChar();
+			System.out.println("you entered: "+chrOption);
+			
+		if(chrOption == 'p' || chrOption == 'P'){
+			con.setBackgroundColor(new Color(53, 101, 77));
+			
+			con.setDrawColor(new Color(187, 165, 61));
+			con.drawRoundRect(50, 350, 100, 200, 50, 50);
+			con.drawRoundRect(200, 350, 100, 200, 50, 50);
+			con.drawRoundRect(350, 350, 100, 200, 50, 50);
+			con.drawRoundRect(500, 350, 100, 200, 50, 50);
+			con.drawRoundRect(650, 350, 100, 200, 50, 50);
+			
+		}else if(chrOption == 'l' || chrOption == 'L'){
+			con.setBackgroundColor(Color.BLACK);
+		}else if(chrOption == 'q' || chrOption == 'Q'){
+			con.setBackgroundColor(Color.BLACK);
+		}else if(chrOption == 'h' || chrOption == 'H'){
+			con.setBackgroundColor(Color.BLACK);
+		}
 	}
 }
