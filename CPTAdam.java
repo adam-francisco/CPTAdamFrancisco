6e93dc39ac83703c8ad259d2ce70dec0faf7c591
adam-francisco <adammatthew.francisco26@ycdsbk12.ca>
2025-06-03 09:15:45 -0400

    Worked on help menu, tweaked card shapes, imported new photo, added secret menu option.

--- CPTAdam.java
+++ CPTAdam.java
@@ -35,10 +35,14 @@ public class CPTAdam{
 		
 		// for determining option on menu
 		chrOption = con.getChar();
-			System.out.println("you entered: "+chrOption);
+		System.out.println("entered letter: "+chrOption);
 			
 		// Play option
 		if(chrOption == 'p' || chrOption == 'P'){
+			
+			int intCash;
+			
+			intCash = 1000;
 			
 			// Page created for name of player
 			con.setBackgroundColor(Color.BLACK);
@@ -58,43 +62,68 @@ public class CPTAdam{
 			
 			// Card slot holders
 			con.setDrawColor(new Color(187, 165, 61));
-			con.fillRoundRect(50, 375, 100, 200, 50, 50);
-			con.fillRoundRect(250, 350, 100, 200, 50, 50);
-			con.fillRoundRect(450, 325, 100, 200, 50, 50);
-			con.fillRoundRect(650, 350, 100, 200, 50, 50);
-			con.fillRoundRect(850, 375, 100, 200, 50, 50);
+			con.fillRoundRect(50, 375, 100, 200, 30, 30);
+			con.fillRoundRect(250, 350, 100, 200, 30, 30);
+			con.fillRoundRect(450, 325, 100, 200, 30, 30);
+			con.fillRoundRect(650, 350, 100, 200, 30, 30);
+			con.fillRoundRect(850, 375, 100, 200, 30, 30);
 			
-			con.fillRoundRect(50, 25, 100, 200, 50, 50);
-			con.fillRoundRect(250, 50, 100, 200, 50, 50);
-			con.fillRoundRect(450, 75, 100, 200, 50, 50);
-			con.fillRoundRect(650, 50, 100, 200, 50, 50);
-			con.fillRoundRect(850, 25, 100, 200, 50, 50);
+			con.fillRoundRect(50, 25, 100, 200, 30, 30);
+			con.fillRoundRect(250, 50, 100, 200, 30, 30);
+			con.fillRoundRect(450, 75, 100, 200, 30, 30);
+			con.fillRoundRect(650, 50, 100, 200, 30, 30);
+			con.fillRoundRect(850, 25, 100, 200, 30, 30);
 			
 			// Card slot filler
 			con.setDrawColor(new Color(53, 101, 77));
-			con.fillRoundRect(60, 385, 80, 180, 50, 50);
-			con.fillRoundRect(260, 360, 80, 180, 50, 50);
-			con.fillRoundRect(460, 335, 80, 180, 50, 50);
-			con.fillRoundRect(660, 360, 80, 180, 50, 50);
-			con.fillRoundRect(860, 385, 80, 180, 50, 50);
+			con.fillRoundRect(60, 385, 80, 180, 25, 25);
+			con.fillRoundRect(260, 360, 80, 180, 25, 25);
+			con.fillRoundRect(460, 335, 80, 180, 25, 25);
+			con.fillRoundRect(660, 360, 80, 180, 25, 25);
+			con.fillRoundRect(860, 385, 80, 180, 25, 25);
 			
-			con.fillRoundRect(60, 35, 80, 180, 50, 50);
-			con.fillRoundRect(260, 60, 80, 180, 50, 50);
-			con.fillRoundRect(460, 85, 80, 180, 50, 50);
-			con.fillRoundRect(660, 60, 80, 180, 50, 50);
-			con.fillRoundRect(860, 35, 80, 180, 50, 50);
+			con.fillRoundRect(60, 35, 80, 180, 25, 25);
+			con.fillRoundRect(260, 60, 80, 180, 25, 25);
+			con.fillRoundRect(460, 85, 80, 180, 25, 25);
+			con.fillRoundRect(660, 60, 80, 180, 25, 25);
+			con.fillRoundRect(860, 35, 80, 180, 25, 25);
 			
 			// Name and dealer strings onto console
 			con.setDrawColor(Color.WHITE);
 			con.drawString("Dealer",465,10);
-			con.drawString(strName ,475,560);
+			con.drawString(strName + " - $" + intCash,440,560);
 			
 		}else if(chrOption == 'l' || chrOption == 'L'){
 			con.setBackgroundColor(Color.BLACK);
+			
 		}else if(chrOption == 'q' || chrOption == 'Q'){
-			con.setBackgroundColor(Color.BLACK);
+			con.closeConsole();
+			
 		}else if(chrOption == 'h' || chrOption == 'H'){
-			con.setBackgroundColor(Color.BLACK);
+			con.setBackgroundColor(Color.WHITE);
+			
+			char chrOption2;
+			
+			BufferedImage imgAces = con.loadImage("Aces.png");
+				con.drawImage(imgAces, 300, 0);
+				
+			con.setDrawColor(Color.BLACK);
+			con.drawString("HELP MENU",445,400);
+			con.drawString("[R] Rules",445,480);	
+			con.drawString("[C] Credits",445,510);
+			
+			con.setDrawColor(new Color(245, 245, 245));
+			con.drawString("[S] ???",445,540);		
+				
+			con.setDrawColor(new Color(205, 205, 205));
+			con.fillRect(400, 450, 200, 5);
+			
+			chrOption2 = con.getChar();
+			System.out.println("entered letter: "+chrOption2);
+			
+			if(chrOption == 'r' || chrOption == 'R'){
+				
+			}
 		}
 	}
 }
